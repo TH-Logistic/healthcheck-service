07cd92ff165508dd2fcb9c8b06bc04e0a0b851bb
trunnghieu01 <work.trunghieu.0107@gmail.com>
2023-05-04 16:32:53 +0700

    UPDATE: Migrate to Feign and use env var

--- src/main/java/com/thlogistic/healthcheck/healthcheckservice/config/ApplicationConfig.java
+++ src/main/java/com/thlogistic/healthcheck/healthcheckservice/config/ApplicationConfig.java
@@ -1,11 +1,30 @@
 package com.thlogistic.healthcheck.healthcheckservice.config;
 
+import com.thlogistic.healthcheck.healthcheckservice.client.AuthorizationClient;
+import feign.Feign;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.okhttp.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfig {
+
+    private static String domainUrl = System.getenv("DOMAIN_URL");
+
+    public static final String AUTHORIZATION_BASE_URL = "http://" + domainUrl + ":8000";
+
+    @Bean
+    public AuthorizationClient authorizationClient() {
+        return Feign.builder()
+                .client(new OkHttpClient())
+                .encoder(new GsonEncoder())
+                .decoder(new GsonDecoder())
+                .target(AuthorizationClient.class, AUTHORIZATION_BASE_URL);
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
--- src/main/java/com/thlogistic/healthcheck/healthcheckservice/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/thlogistic/healthcheck/healthcheckservice/interceptor/AuthenticationInterceptor.java
@@ -2,14 +2,12 @@ package com.thlogistic.healthcheck.healthcheckservice.interceptor;
 
 import com.thlogistic.healthcheck.healthcheckservice.adapters.dtos.BaseResponse;
 import com.thlogistic.healthcheck.healthcheckservice.aop.exception.UnauthorizedException;
+import com.thlogistic.healthcheck.healthcheckservice.client.AuthorizationClient;
+import com.thlogistic.healthcheck.healthcheckservice.client.PermissionDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.io.IOException;
@@ -19,34 +17,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthenticationInterceptor implements HandlerInterceptor {
     private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String AUTHORIZATION_URL = "http://www.thinhlh.com:8000/check-permissions";
-    private final RestTemplate restTemplate;
+    private final AuthorizationClient authorizationClient;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         if (request.getMethod().equals("OPTIONS")) {
             return true;
         }
+
         String token = request.getHeader(AUTHORIZATION_HEADER);
+        List<String> roles = List.of("admin");
 
         if (token != null) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set(AUTHORIZATION_HEADER, token);
-
-            List<String> body = List.of("admin");
-
-            HttpEntity<List<String>> authRequest = new HttpEntity<>(body, headers);
-
             try {
-                ResponseEntity<BaseResponse> authResponse = restTemplate.postForEntity(AUTHORIZATION_URL, authRequest, BaseResponse.class);
-                if (authResponse.getBody() == null || !authResponse.getBody().getSuccess()) {
+                BaseResponse<PermissionDto> permissionResponse = authorizationClient.checkPermission(token, roles);
+                if (!permissionResponse.getSuccess()) {
                     throw new UnauthorizedException("Invalid token credential");
                 }
             } catch (Exception e) {
                 throw new UnauthorizedException("Invalid token credential");
             }
         } else {
-            throw new UnauthorizedException("Unauthenticated");
+            throw new UnauthorizedException("Invalid token credential");
         }
         return true;
     }
